dd821a3d35cae68491363c590c52e9e2529da66e
sseonmo <mo3509@gmail.com>
2019-08-19 01:38:10 +0900

    [19.08.19] 컴포넌트 생성 및 등록

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader'
+  import TodoInput from './components/TodoInput'
+  import TodoList from './components/TodoList'
+  import TodoFooter from './components/TodoFooter'
+  export default {
+    components :{
+    //컴포넌트 태그명 : 컴포넌트 내용
+      'TodoHeader' : TodoHeader,
+      'TodoInput' : TodoInput,
+      'TodoList' : TodoList,
+      'TodoFooter' : TodoFooter,
+    }
+}
+</script>
+
+<style>
+
+</style>
